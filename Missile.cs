4e2e30970a2a52179ef12e8f747ed0ed3476a75c
Bordyr21 <165457269+Bordyr21@users.noreply.github.com>
2025-12-20 18:20:36 +0700

    Add files via upload

---
+++ Missile.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+	public float speed;
+
+	void Start()
+	{
+		Destroy(gameObject, 3);
+	}
+
+    void Update()
+    {
+       // лон огненного шара уничтожаетс€ через 3 секунды
+        Destroy(gameObject, 3);
+
+          // аждый кадр позици€ шара обновл€етс€ на произведение вектора движени€ вперЄд (0,0,1),
+        //скорости движени€ шара и значение разницы в секундах между последним и текущим кадрами
+
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+ 
+    void OnTriggerEnter(Collider other) {
+ 
+        //¬раг уничтожаетс€
+        Enemy enemy = other.GetComponent<Enemy>();
+        Destroy(enemy.gameObject);
+ 
+        //—нар€д уничтожаетс€
+        Destroy(gameObject);
+ 
+    }
+ 
+}
